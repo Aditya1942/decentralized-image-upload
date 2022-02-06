0c0f1d9eb1d646d3f70fa0077a2ebcf2f755d7dd
Aditya1942 <parmaraditya1942@gmail.com>
2022-02-06 18:51:16 +0530

    image hash storing in smart comtract added

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,20 @@
 import React, { Component } from "react";
 import SimpleStorageContract from "./contracts/SimpleStorage.json";
-import getWeb3 from "./getWeb3";import ipfs from './ipfs'
+import getWeb3 from "./getWeb3"; import ipfs from './ipfs'
 
 import "./App.css";
 
 class App extends Component {
- 
-constructor(props) {
+
+  constructor(props) {
     super(props)
 
     this.state = {
-    storageValue: 0,
-    web3: null, 
-    accounts: null, 
-    contract: null ,  
-    buffer: null,
-    ipfsHash: '',
+      web3: null,
+      accounts: null,
+      contract: null,
+      buffer: null,
+      ipfsHash: '',
     }
     this.captureFile = this.captureFile.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
@@ -39,7 +38,7 @@ constructor(props) {
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance });
+      this.setState({ web3, accounts, contract: instance }, this.runExample);
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -53,15 +52,16 @@ constructor(props) {
     const { accounts, contract } = this.state;
 
     // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
+    // await contract.methods.set(5).send({ from: accounts[0] });
 
     // Get the value from the contract to prove it worked.
     const response = await contract.methods.get().call();
 
     // Update state with the result.
-    this.setState({ storageValue: response });
+    console.log('response', response)
+    this.setState({ ipfsHash: response });
   };
-   captureFile(event) {
+  captureFile(event) {
     event.preventDefault()
     const file = event.target.files[0]
     console.log(file);
@@ -75,16 +75,17 @@ constructor(props) {
 
   onSubmit(event) {
     event.preventDefault()
-    ipfs.files.add(this.state.buffer, (error, result) => {
-      if(error) {
+    const { accounts, contract } = this.state;
+
+    ipfs.files.add(this.state.buffer, async (error, result) => {
+      if (error) {
         console.error(error)
         return
       }
-      console.log(result);
-      // this.simpleStorageInstance.set(result[0].hash, { from: this.state.account }).then((r) => {
-        return this.setState({ ipfsHash: result[0].hash })
-      //   console.log('ifpsHash', this.state.ipfsHash)
-      // })
+      await contract.methods.set(result[0].hash).send({ from: accounts[0] });
+
+      this.setState({ ipfsHash: result[0].hash })
+      return console.log('ifpsHash', this.state.ipfsHash)
     })
   }
 
@@ -94,17 +95,19 @@ constructor(props) {
     }
     return (
       <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>IPFS file upload example</h2>
-     <h1>Your Image</h1>
-              <p>{'This image is stored on IPFS & The Ethereum Blockchain!'}</p>
-              <img style={{height:'200px'}} src={`https://ipfs.io/ipfs/${this.state.ipfsHash}`} alt=""/>
-              <h2>Upload Image</h2>
-              <form onSubmit={this.onSubmit} >
-                <input type='file' onChange={this.captureFile} />
-                <input type='submit' />
-              </form>
+
+        <h2 className="text-3xl font-bold underline">IPFS file upload with Ethereum contract example</h2>
+        <h1>Your Image</h1>
+        <p>{'This image is stored on IPFS & The Ethereum Blockchain!'}</p>
+        <div className="flex justify-center align-middle">
+
+          <img className="h-40 " src={`https://ipfs.io/ipfs/${this.state.ipfsHash}`} alt="" />
+        </div>
+        <h2>Upload Image</h2>
+        <form onSubmit={this.onSubmit} >
+          <input type='file' onChange={this.captureFile} />
+          <input type='submit' />
+        </form>
       </div>
     );
   }
